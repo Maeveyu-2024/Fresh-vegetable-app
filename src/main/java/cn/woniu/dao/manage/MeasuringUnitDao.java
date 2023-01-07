a08c28a6a30e108808042701bbdbbece183cdeac
zhang-ao-maple <1353329337@qq.com>
2023-01-07 18:32:37 +0800

    区域管理板块

--- src/main/java/cn/woniu/dao/manage/MeasuringUnitDao.java
+++ src/main/java/cn/woniu/dao/manage/MeasuringUnitDao.java
@@ -1,8 +1,11 @@
 package cn.woniu.dao.manage;
 
 
+import cn.woniu.entity.manage.Area;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import cn.woniu.entity.manage.MeasuringUnit;
+
+import java.util.List;
 
 /**
  * (MeasuringUnit)表数据库访问层
@@ -10,7 +13,14 @@ import cn.woniu.entity.manage.MeasuringUnit;
  * @author makejava
  * @since 2023-01-05 11:38:58
  */
-public interface MeasuringUnitDao extends BaseMapper<MeasuringUnit> {
-
+public interface MeasuringUnitDao  {
+    List<MeasuringUnit> queryMeasuringUnitList(String name);//查询计量单位表数据
+    void addMeasuringUnit(MeasuringUnit measuringUnit);//添加计量单位
+    void updateMeasuringUnit(MeasuringUnit measuringUnit);//修改计量单位表
+    void delMeasuringUnit(Long id);//逻辑删除计量单位表数据
+    void stopStatus(Long id);//停用计量单位
+    void openStatus(Long id);//开启计量单位
+    List<MeasuringUnit> queryAlList();//查询全部计量单位
+    Integer queryOtherMUById(Long id);//查询名下使用者数量
 
 }
--- src/main/java/cn/woniu/service/manage/MeasuringUnitService.java
+++ src/main/java/cn/woniu/service/manage/MeasuringUnitService.java
@@ -1,6 +1,11 @@
 package cn.woniu.service.manage;
 
 
+import cn.woniu.entity.manage.MeasuringUnit;
+import cn.woniu.utils.ResponseResult;
+
+import java.util.List;
+
 /**
  * (MeasuringUnit)表服务接口
  *
@@ -8,6 +13,14 @@ package cn.woniu.service.manage;
  * @since 2023-01-05 11:38:58
  */
 public interface MeasuringUnitService {
+    ResponseResult<?> queryMeasuringUnitList(String name ,Integer pageNo, Integer pageSize);//查询计量单位表数据
+    ResponseResult<?>  addMeasuringUnit(MeasuringUnit measuringUnit);//添加计量单位
+    ResponseResult<?>  updateMeasuringUnit(MeasuringUnit measuringUnit);//修改计量单位表
+    ResponseResult<?>  delMeasuringUnit(Long id);//逻辑删除计量单位表数据
+    ResponseResult<?>  stopStatus(Long id);//停用计量单位
+    ResponseResult<?>  openStatus(Long id);//开启计量单位
+    ResponseResult<?>  queryAlList();//查询全部计量单位
+    ResponseResult<?>  queryOtherMUById(Long id);//查询名下使用者数量
 
 }
 
