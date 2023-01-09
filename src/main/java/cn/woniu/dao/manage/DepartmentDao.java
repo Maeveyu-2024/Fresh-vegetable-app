22732896fbcef34af9aacb570116693ce518cde6
xy <843102880@qq.com>
2023-01-09 14:20:37 +0800

    '部门管理的完成和员工管理的完善'

--- src/main/java/cn/woniu/dao/manage/DepartmentDao.java
+++ src/main/java/cn/woniu/dao/manage/DepartmentDao.java
@@ -1,8 +1,11 @@
 package cn.woniu.dao.manage;
 
 
+import cn.woniu.entity.manage.Employee;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import cn.woniu.entity.manage.Department;
+
+import java.util.List;
 
 /**
  * (Department)表数据库访问层
@@ -11,6 +14,12 @@ import cn.woniu.entity.manage.Department;
  * @since 2023-01-05 11:38:50
  */
 public interface DepartmentDao extends BaseMapper<Department> {
-
-
+    //查询部门列表
+    List<Department> selectDeptList();
+    //更新部门状态
+    int updateDeptStatus(Integer id, Integer status);
+    //通过传入的name查询数据库是否存在
+    Department selectDeptByName(String name);
+    //删除和禁用时需要将部门ID放到员工表查询是否有员工
+    List<Employee> selectEmployeeByDeptId(Integer id);
 }
--- src/main/java/cn/woniu/service/manage/impl/DepartmentServiceImpl.java
+++ src/main/java/cn/woniu/service/manage/impl/DepartmentServiceImpl.java
@@ -1,9 +1,17 @@
 package cn.woniu.service.manage.impl;
 
 import cn.woniu.dao.manage.DepartmentDao;
+import cn.woniu.entity.manage.Department;
+import cn.woniu.entity.manage.Employee;
 import cn.woniu.service.manage.DepartmentService;
+import cn.woniu.utils.ResponseResult;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 /**
@@ -17,5 +25,43 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Autowired(required = false)
     private DepartmentDao departmentDao;
+
+    @Override
+    public ResponseResult<?> queryDeptList(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        List<Department> departments = departmentDao.selectDeptList();
+        PageInfo<Employee> pageInfo = new PageInfo(departments);
+        return new ResponseResult<>().ok(pageInfo);
+    }
+
+    @Override
+    public ResponseResult<?> updateDeptStatus(Integer id, Integer status) {
+        List<Employee> employees = departmentDao.selectEmployeeByDeptId(id);
+        if(employees.size() != 0){
+            return new ResponseResult<>().ok("该部门下有员工");
+        } else {
+            return new ResponseResult<>().ok(departmentDao.updateDeptStatus(id,status));
+        }
+    }
+
+    @Override
+    public ResponseResult<?> queryDeptName(String name) {
+        Department department = departmentDao.selectDeptByName(name);
+        return new ResponseResult<>().ok(department);
+    }
+
+    @Override
+    public ResponseResult<?> addDept(String name) {
+        return new ResponseResult<>().ok(departmentDao.insert(new Department(name,LocalDate.now())));
+    }
+
+    @Override
+    public ResponseResult<?> updateDeptInfo(String id, String name) {
+        Department department = new Department();
+        department.setId(id);
+        department.setName(name);
+        department.setUpdateTime(LocalDate.now());
+        return new ResponseResult<>().ok(departmentDao.updateById(department));
+    }
 }
 
