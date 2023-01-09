bcee40cdabccb48ea29450525af8613b40314124
xy <843102880@qq.com>
2023-01-09 09:47:43 +0800

    '员工管理'

--- src/main/java/cn/woniu/dao/manage/GoodsDao.java
+++ src/main/java/cn/woniu/dao/manage/GoodsDao.java
@@ -4,6 +4,8 @@ package cn.woniu.dao.manage;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import cn.woniu.entity.manage.Goods;
 
+import java.util.List;
+
 /**
  * (Goods)表数据库访问层
  *
@@ -11,6 +13,4 @@ import cn.woniu.entity.manage.Goods;
  * @since 2023-01-05 11:38:52
  */
 public interface GoodsDao extends BaseMapper<Goods> {
-
-
 }
