e06ce1d507e7fc544ffb0a791bae062ae66fa455
xy <843102880@qq.com>
2023-01-10 15:08:06 +0800

    '权限管理、权限分配'

--- src/main/java/cn/woniu/controller/authority/AuthorityController.java
+++ src/main/java/cn/woniu/controller/authority/AuthorityController.java
@@ -2,9 +2,14 @@ package cn.woniu.controller.authority;
 
 
 import cn.woniu.service.authority.AuthorityService;
+import cn.woniu.utils.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * (Authority)表控制层
@@ -20,4 +25,20 @@ public class AuthorityController {
      */
     @Autowired
     private AuthorityService authorityService;
+    @RequestMapping("auth")
+    public ResponseResult<?> queryAuthList(String username){
+        return authorityService.queryAuthorityList(username);
+    }
+    @RequestMapping("authList")
+    public ResponseResult<?> queryAllAuthList(){
+        return authorityService.queryAllAuthority();
+    }
+    @RequestMapping("allAuthList")
+    public ResponseResult<?> selectAllAuthList(){
+        return authorityService.selectAllAuthority();
+    }
+    @PostMapping("updateAuth")
+    public ResponseResult<?> updateAuthByUid(@RequestBody List<Integer> ids, Integer id){
+        return authorityService.updateAuthByUid(ids,id);
+    }
 }
--- src/main/java/cn/woniu/service/authority/AuthorityService.java
+++ src/main/java/cn/woniu/service/authority/AuthorityService.java
@@ -1,6 +1,10 @@
 package cn.woniu.service.authority;
 
 
+import cn.woniu.utils.ResponseResult;
+
+import java.util.List;
+
 /**
  * (Authority)表服务接口
  *
@@ -8,6 +12,9 @@ package cn.woniu.service.authority;
  * @since 2023-01-05 11:38:48
  */
 public interface AuthorityService {
-
+    ResponseResult<?> queryAuthorityList(String username);
+    ResponseResult<?> queryAllAuthority();
+    ResponseResult<?> selectAllAuthority();
+    ResponseResult<?> updateAuthByUid(List<Integer> ids, Integer id);
 }
 
